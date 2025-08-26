a110cc57c6627eaba55f5a54d28d7336f2359a4e
sara-18Git <saraabigailrobles9@gmail.com>
2025-08-26 18:46:12 -0300

    agrege la carpeta pages junto con el archivo HomePage

---
+++ proyecto2/src/components/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la página de inicio</h1>
+      <p>Este es el contenido de la página principal de tu sitio.</p>
+    </div>
+  );
+};
+
+export default HomePage;
