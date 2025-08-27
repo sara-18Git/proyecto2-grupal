a425f5c83179d46b6d4bf024843a150785b7b089
Damián Osores <damianosores@hotmail.com>
2025-08-27 17:59:38 -0300

    cree la estructura de la pagina, contenido base

--- proyecto2/src/main.jsx
+++ proyecto2/src/main.jsx
@@ -1,11 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // Importa los componentes de ruteo
+import './index.css';
+import App from './App.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
+import SobreNosotros from "./pages/SobreNosotros.jsx";
 
+// Define el enrutador con las rutas de tu aplicación
+const router = createBrowserRouter([
+  {
+    path: "/", // Ruta para la página principal
+    element: <App />,
+  },
+  {
+    path: "/nosotros", // Ruta para la página "Sobre Nosotros"
+    element: <SobreNosotros />,
+  },
+]);
+
+// Renderiza la aplicación usando el RouterProvider
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
