0d5cc1d7312b7d4a00917d2d45dcb46ff0e428d9
sara-18Git <saraabigailrobles9@gmail.com>
2025-09-02 17:14:06 -0300

    agrege un segundo carrusel

--- proyecto2/src/pages/HomePage.jsx
+++ proyecto2/src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
-
 import React from "react";
-import Carruseles from "../components/carruseles";
+import Carruseles from "../components/Carruseles";
 import Carrusel from "../components/Carrusel";
 import Beneficios from "../components/Beneficios";
 import Destacado from "../components/Destacado";
-
+import Carruseles2 from "../components/carruseles2";
 
 const HomePage = () => {
   return (
@@ -13,10 +12,10 @@ const HomePage = () => {
       <Carruseles></Carruseles>
       <br />
       <Beneficios></Beneficios>
-
       <br />
       <Destacado></Destacado>
-
+      <br />
+      <Carruseles2></Carruseles2>
     </div>
   );
 };
