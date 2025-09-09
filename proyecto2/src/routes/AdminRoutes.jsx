6115523df9a1e16883ac78a77f7438e5f786b2fb
Amelia-0705 <maantonib@yahoo.com.ar>
2025-09-09 11:16:42 -0300

    corrijo links de footer, cambio background de color para una mejor visibilidad, agrego scroll To top para que suba al inicio cada vez que haga link a otra pagina (#30)

--- proyecto2/src/routes/AdminRoutes.jsx
+++ proyecto2/src/routes/AdminRoutes.jsx
@@ -5,7 +5,7 @@ const AdminRoutes = () => {
   //  buscar si hay un usuario en session
 
   const user = JSON.parse(sessionStorage.getItem("usuario")) || null;
-  console.log(user.email);
+  console.log(user?.email);
   if (user?.email === "saraabigailrobles9@gmail.com") {
     return <Outlet />;
   } else {
